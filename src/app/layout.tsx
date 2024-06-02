0a459ffbf9de7afe4d078808c97749a422e11172
igna <igna@femboy.band>
2024-06-02 06:35:12 +0200

    [refactor] remove useless imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css"
 
 import type { Metadata, Viewport } from "next"
 import localFont from "next/font/local"
-import Script from "next/script"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import * as React from "react"
 import { useEffect } from "react"
 import { ReactLenis } from "@studio-freight/react-lenis"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
